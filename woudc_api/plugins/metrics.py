a17981d98e7fa9a31a8c55d00ce012dc0cacf95a
victoriarspada <victoria.spada@canada.ca>
2021-09-02 09:09:10 -0400

    Change bbox input from string to array
    Change bbox input from string to array

--- woudc_api/plugins/metrics.py
+++ woudc_api/plugins/metrics.py
@@ -86,6 +86,14 @@ PROCESS_SETTINGS = {
             'minOccurs': 1,
             'maxOccurs': 1
         },
+        'bbox': {
+            'title': 'Bounding Box',
+            'schema': {
+                'type': 'array',
+            },
+            'minOccurs': 0,
+            'maxOccurs': 1
+        },
         'dataset': {
             'title': 'Dataset Filter',
             'schema': {
@@ -262,9 +270,7 @@ class MetricsProcessor(BaseProcessor):
             if network is not None:
                 filters.append({'properties.instrument_name.raw': network})
             if bbox is not None:
-                bbox_vals = bbox.split(',')
-                west_long, east_long = float(bbox_vals[0]), float(bbox_vals[2])
-                south_lat, north_lat = float(bbox_vals[1]), float(bbox_vals[3])
+                minx, miny, maxx, maxy = [float(b) for b in bbox]
             field = 'properties.start_datetime'
         else:
             if dataset is not None:
@@ -278,9 +284,7 @@ class MetricsProcessor(BaseProcessor):
             if network is not None:
                 filters.append({'properties.instrument_name.raw': network})
             if bbox is not None:
-                bbox_vals = bbox.split(',')
-                west_long, east_long = float(bbox_vals[0]), float(bbox_vals[2])
-                south_lat, north_lat = float(bbox_vals[1]), float(bbox_vals[3])
+                minx, miny, maxx, maxy = [float(b) for b in bbox]
             field = 'properties.timestamp_date'
 
         conditions = [{'term': body} for body in filters]
@@ -323,8 +327,8 @@ class MetricsProcessor(BaseProcessor):
                                                 'shape': {
                                                     'type': 'envelope',
                                                     'coordinates': [
-                                                        [west_long, north_lat],
-                                                        [east_long, south_lat]
+                                                        [minx, maxy],
+                                                        [maxx, miny]
                                                     ]
                                                 },
                                                 'relation': 'within'
